1735eab47fc5d2d805a259b086f3814218f3ef49
zwergdev <zwergdev@gmail.com>
2024-01-22 15:19:50 +0100

    feat: add analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from 'next/font/google'
 import './globals.css'
 import { Header } from '@/components/home/header'
 import { Footer } from '@/components/home/footer'
+import { Analytics } from '@vercel/analytics/react'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -50,6 +51,7 @@ export default function RootLayout({
           <div className="grow" />
           <Footer />
         </div>
+        <Analytics />
       </body>
     </html>
   )
